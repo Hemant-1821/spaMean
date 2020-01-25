ddd73530eb27d000dcbb9e2bd83cc7ae6efe4d80
Hemant Singh <singh.hemant9583@gmail.com>
2020-01-25 23:50:51 +0530

    Fifth update with form validation and unique contact entry

--- public/js/controllers/AddCtrl.js
+++ public/js/controllers/AddCtrl.js
@@ -1,7 +1,22 @@
 angular.module('AddCtrl',[]).controller('AddController',function($scope,$http){
-  $scope.send_data=function(){
-    $http.post('/api/abouts',$scope.form_data).then(function(respo){
-      console.log(respo);
-    })
+
+  $scope.check_data = function(){
+      $http.get('/api/abouts').then(function(res){
+        $scope.check_data = res.data;
+        for (let value of $scope.check_data) {
+          if(value.phone==$scope.form_data.phone){
+            $scope.found = 1;
+          }
+        }
+        if($scope.found!=1){
+            $http.post('/api/abouts',$scope.form_data).then(function(respo){
+              console.log(respo);
+              $scope.message = "User added";
+            })
+          }
+        else{
+          $scope.message = "Phone Number already exists!!";
+        }
+      });
   }
 });
--- public/js/controllers/ReadCtrl.js
+++ public/js/controllers/ReadCtrl.js
@@ -1,6 +1,6 @@
 angular.module('ReadCtrl',[]).controller('ReadController',function($scope,$http){
   $scope.rec_data=function(){
-    $http.get('/api/abouts1').then(function(res){
+    $http.get('/api/abouts').then(function(res){
       $scope.con_data = res.data;
     });
 }
